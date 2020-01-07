17cc101174b32b23b698c9bf186620a01972ea42
Sky Jones <rsajones94@gmail.com>
2020-01-07 00:35:22 -0600

    sensitivity

--- master.py
+++ master.py
@@ -1,9 +1,15 @@
+import itertools
+import pandas as pd
+
 from skimage import io
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 from matplotlib.cm import ScalarMappable
+from matplotlib.lines import Line2D
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.path import Path
+from sklearn import metrics
 
 from tree_extraction import extract_crowns_from_dhm
 from neighborhood_funcs import *
@@ -16,11 +22,12 @@ a "GLCM" but for a point cloud instead of rasters. "directionality" to disorder
 """
 
 use_dhm = True
+
 trees_1 = True
 trees_2 = False
 
 save = False
-plot_planted = True
+sensitivity = True
 
 if use_dhm:
     if trees_1:
@@ -36,6 +43,8 @@ if use_dhm:
         punish_out_of_hull = False
         euc = False
         reorientation = None
+
+        plot_planted = True
 
         max_sigma = 30
         min_sigma = 6
@@ -90,6 +99,8 @@ if use_dhm:
         num_sigma = 10
         threshold = 5
         overlap = 0.5
+
+        plot_planted = False
 
         thresh = 0.5
     else:
@@ -167,91 +178,175 @@ else:
 
     # pts = decimate_grid(pts, 0.7)
 
-scores, neighborhoods, scatter_key, score_key = point_disorder_index(pts[:, 0:2],
-                                                                     neighbor_search_dist,
-                                                                     ka=ka,
-                                                                     coop=coop,
-                                                                     punishment=punishment,
-                                                                     punish_out_of_hull=punish_out_of_hull,
-                                                                     euclidean=euc,
-                                                                     reorient_tol=reorientation)
+if sensitivity:
+    pts_out = r'C:\Users\rsjon_000\Documents\point-disorder\point_disorder_paper\figures\pts_as_fxn_of_radius.png'
+    sens_out = r'C:\Users\rsjon_000\Documents\point-disorder\point_disorder_paper\figures\sensitivity.csv'
+
+    radii = np.arange(30, 160, 10)
+    kas = np.arange(1, 22, 2)
+
+    r_k = [(a, b) for a in radii for b in kas]
+    kappas = []
+
+    is_in = []
+    for shape in aoi:
+        p = Path(shape)
+        is_in.append(p.contains_points(pts[:,0:2]))
+    is_in = np.vstack(is_in)
+    is_in = np.array([any(is_in[:,i]) for i,_ in enumerate(is_in[0])])
+
+    kappa_df = pd.DataFrame(index=radii, columns=kas)
+    num_neighbors = pd.Series(index=radii)
+    num_points = pd.Series(index=radii)
+
+    for i,(r,k) in enumerate(r_k):
+        print(f'\n\nSENSITIVITY: {i+1} of {len(r_k)}\n\n')
+
+        scores, neighborhoods, scatter_key, score_key = point_disorder_index(pts[:, 0:2],
+                                                                             r,
+                                                                             ka=k,
+                                                                             coop=coop,
+                                                                             punishment=punishment,
+                                                                             punish_out_of_hull=punish_out_of_hull,
+                                                                             euclidean=euc,
+                                                                             reorient_tol=reorientation)
+
+        scores = np.array(scores)
+        under_thresh = np.array([score <= thresh for score in scores])
+        mask = ~np.isnan(scores)
+
+        good_scores = scores[mask]
+        good_thresh = under_thresh[mask]
+        good_is_in = is_in[mask]
+
+        kappa = metrics.cohen_kappa_score(good_is_in, good_thresh)
+        kappas.append(kappa)
+
+        kappa_df[k][r] = kappa
+
+        neighbs = np.mean([len(pt['neighbors']) for pt in neighborhoods])
+        num_neighbors[r] = neighbs
+
+        num_points[r] = len(good_scores)
+
+    fig, ax = plt.subplots(2, 1, figsize=(8,12))
+    ax[0].plot(radii, num_points)
+    ax[1].plot(radii, num_neighbors)
+
+    ax[0].set_title(f'Number of points with at least one neighbor')
+    ax[1].set_title(f'Average number of neighbors')
+
+    ax[0].set_xlabel('Neighborhood radius (m)')
+    ax[1].set_xlabel('Neighborhood radius (m)')
+
+    ax[0].set_ylabel('Points')
+    ax[1].set_ylabel('Neighbors')
+
+    plt.tight_layout()
+    fig.savefig(pts_out)
+
+    kappa_df.to_csv(sens_out)
 
 
-print('Drawing')
-color_map = cm.get_cmap('RdYlGn_r')
-norm = plt.Normalize(0, 1)
-sm = ScalarMappable(norm=norm, cmap=color_map)
-if use_dhm:
-    fig, ax = plt.subplots(1, 2, figsize=(12, 8))
 
-    ax[0].imshow(sub_image_gray, cmap='gray')
-    ax[1].imshow(sub_image_gray, cmap='gray')
-    ax[1].set_title(f'r={neighbor_search_dist}, ka={ka}, coop={coop}\n'
-                    f'punishment={punishment}, punish_out_of_hull={punish_out_of_hull}\n'
-                    f'euc={euc}, reorientation={reorientation}')
-    ax[0].set_title(f'Threshold = {thresh}')
-    # plt.tight_layout()
 
-    for i, tree in enumerate(pts):
-        x, y, r = tree
-        raw_col = scores[i]
-        col = color_map(scores[i])
-        if raw_col > thresh:
-            threshcol = 'red'
-        else:
-            threshcol = 'green'
-
-        if np.isnan(raw_col):
-            col = 'dodgerblue'
-            threshcol = 'dodgerblue'
-        c = plt.Circle((x, y), radius=5, color=col, linewidth=1, fill=True)
-        ax[1].add_patch(c)
-        c = plt.Circle((x, y), radius=5, color=threshcol, linewidth=1, fill=True)
-        ax[0].add_patch(c)
-        # ax.annotate(i, (x, y))
-    ax[0].set_aspect('equal')
-    ax[1].set_aspect('equal')
-
-    if plot_planted:
-        for shape in aoi:
-            shape = np.vstack([shape,shape[0]])
-            ax[0].plot(shape[:,0], shape[:,1], color='purple', linewidth=2)
-            ax[1].plot(shape[:,0], shape[:,1], color='purple', linewidth=2)
-
-    cbar = fig.colorbar(sm, ax=ax, fraction=0.02)
-    cbar.ax.set_title('IoD')
 
 else:
-    fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-    ax.scatter(pts[:, 0], pts[:, 1], c=scores, cmap=color_map, vmin=0, vmax=1, edgecolors='black')
-    # for i,(x,y) in enumerate(pts):
-    #    ax.annotate(i, (x, y))
-    ax.set_title(f'r={neighbor_search_dist}, ka={ka}, coop={coop}\n'
-                 f'punishment={punishment}, punish_out_of_hull={punish_out_of_hull}\n'
-                 f'euc={euc}, reorientation={reorientation}')
-    ax.set_aspect('equal')
-    plt.tight_layout()
+    scores, neighborhoods, scatter_key, score_key = point_disorder_index(pts[:, 0:2],
+                                                                         neighbor_search_dist,
+                                                                         ka=ka,
+                                                                         coop=coop,
+                                                                         punishment=punishment,
+                                                                         punish_out_of_hull=punish_out_of_hull,
+                                                                         euclidean=euc,
+                                                                         reorient_tol=reorientation)
 
-    cbar = fig.colorbar(sm, ax=ax)
-    cbar.ax.set_title('IoD')
+    print('Drawing')
+    color_map = cm.get_cmap('RdYlGn_r')
+    norm = plt.Normalize(0, 1)
+    sm = ScalarMappable(norm=norm, cmap=color_map)
+    if use_dhm:
+        fig, ax = plt.subplots(1, 2, figsize=(12, 8))
 
-plt.show()
-if save:
-    fig.savefig(outname)
-    plt.close()
+        ax[0].imshow(sub_image_gray, cmap='gray')
+        ax[1].imshow(sub_image_gray, cmap='gray')
+        ax[1].set_title(f'r={neighbor_search_dist}, ka={ka}, coop={coop}\n'
+                        f'punishment={punishment}, punish_out_of_hull={punish_out_of_hull}\n'
+                        f'euc={euc}, reorientation={reorientation}')
+        ax[0].set_title(f'Threshold = {thresh}')
+        # plt.tight_layout()
 
-"""
-ptn = 346
-distance_metric = lambda p1, p2: score_distance_p1p2(p1, p2, ka, coop)
-for neighbor in neighborhoods[ptn]['neighbors']:
-    compare_scatters(neighborhoods[ptn]['coords'],neighborhoods[neighbor]['coords'], plot=True, distance_metric=distance_metric, reorient_tol=0.01)
+        line1 = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor="red")
+        line2 = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor="green")
+        line3 = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor="dodgerblue")
+        lines = [line1, line2, line3]
+        line_names = ['Above threshold ("disordered")', 'Below threshold ("ordered")', 'Insufficient neighborhood']
+        if plot_planted:
+            lines.append(Line2D(range(1), range(1), color="purple"))
+            line_names.append('Planted area')
 
-if not use_dhm:
-    fig, ax = plt.subplots(1, 2)
-    ax[0].scatter(pts[:, 0], pts[:, 1], c=scores, cmap=color_map, vmin=0, vmax=1, edgecolors='black')
-    ax[0].set_aspect('equal')
-    ax[1].scatter(d, scores)
+        ax[0].legend(lines, line_names, numpoints=1, loc=1, prop={'size': 5})
+
+        for i, tree in enumerate(pts):
+            x, y, r = tree
+            raw_col = scores[i]
+            col = color_map(scores[i])
+            if raw_col > thresh:
+                threshcol = 'red'
+            else:
+                threshcol = 'green'
+
+            if np.isnan(raw_col):
+                col = 'dodgerblue'
+                threshcol = 'dodgerblue'
+            c = plt.Circle((x, y), radius=5, color=col, linewidth=1, fill=True)
+            ax[1].add_patch(c)
+            c = plt.Circle((x, y), radius=5, color=threshcol, linewidth=1, fill=True)
+            ax[0].add_patch(c)
+            # ax.annotate(i, (x, y))
+        ax[0].set_aspect('equal')
+        ax[1].set_aspect('equal')
+
+        if plot_planted:
+            for shape in aoi:
+                shape = np.vstack([shape,shape[0]])
+                ax[0].plot(shape[:,0], shape[:,1], color='purple', linewidth=2)
+                ax[1].plot(shape[:,0], shape[:,1], color='purple', linewidth=2)
+
+        cbar = fig.colorbar(sm, ax=ax, fraction=0.02)
+        cbar.ax.set_title('IoD')
+
+    else:
+        fig, ax = plt.subplots(1, 1, figsize=(12, 8))
+        ax.scatter(pts[:, 0], pts[:, 1], c=scores, cmap=color_map, vmin=0, vmax=1, edgecolors='black')
+        # for i,(x,y) in enumerate(pts):
+        #    ax.annotate(i, (x, y))
+        ax.set_title(f'r={neighbor_search_dist}, ka={ka}, coop={coop}\n'
+                     f'punishment={punishment}, punish_out_of_hull={punish_out_of_hull}\n'
+                     f'euc={euc}, reorientation={reorientation}')
+        ax.set_aspect('equal')
+        plt.tight_layout()
+
+        cbar = fig.colorbar(sm, ax=ax)
+        cbar.ax.set_title('IoD')
+
     plt.show()
-"""
+    if save:
+        fig.savefig(outname)
+        plt.close()
 
-print('Done')
+    """
+    ptn = 346
+    distance_metric = lambda p1, p2: score_distance_p1p2(p1, p2, ka, coop)
+    for neighbor in neighborhoods[ptn]['neighbors']:
+        compare_scatters(neighborhoods[ptn]['coords'],neighborhoods[neighbor]['coords'], plot=True, distance_metric=distance_metric, reorient_tol=0.01)
+    
+    if not use_dhm:
+        fig, ax = plt.subplots(1, 2)
+        ax[0].scatter(pts[:, 0], pts[:, 1], c=scores, cmap=color_map, vmin=0, vmax=1, edgecolors='black')
+        ax[0].set_aspect('equal')
+        ax[1].scatter(d, scores)
+        plt.show()
+    """
+
+    print('Done')
