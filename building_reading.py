582d1ad49d0dcf7260662aa2d0f7cf28ac7a37cd
Sky Jones <rsajones94@gmail.com>
2020-01-26 02:16:33 -0600

    no message

--- building_reading.py
+++ building_reading.py
@@ -7,6 +7,8 @@ from matplotlib.cm import ScalarMappable
 from numpy import linspace
 import geopandas as gpd
 from sklearn import metrics
+from matplotlib.lines import Line2D
+import pylab as pl
 
 from tree_extraction import extract_crowns_from_dhm
 from neighborhood_funcs import *
@@ -25,11 +27,13 @@ file = base_file + '.shp'
 df = gpd.read_file(file)
 filt = df
 
+# lockeland
 #25, 7, 3 = .33
 #23, 7, 3 = .36
 #23, 8, 3 = .36
 #21, 7, 3 = .38
 #21, 7, 5 = .39
+
 
 pars = {'neighbor_search_dist': 21,
                'ka': 7,
@@ -61,36 +65,26 @@ scores, neighborhoods, scatter_key, score_key = point_disorder_index(pts[:, 0:2]
                                                                      reorient_tol=reorientation)
 
 
-fig, ax = plt.subplots(1, 1, figsize=(16, 8))
-color_map = cm.get_cmap('RdYlGn_r')
-im1 = ax.scatter(pts[:, 0], pts[:, 1], c=scores, cmap=color_map, vmin=0, vmax=1, edgecolors='black')
-# for i,(x,y) in enumerate(pts):
-#    ax.annotate(i, (x, y))
-ax.set_title(f'r={neighbor_search_dist}, ka={ka}, coop={coop}\n'
-                f'punishment={punishment}, punish_out_of_hull={punish_out_of_hull}\n'
-                f'euc={euc}, reorientation={reorientation}')
-ax.set_aspect('equal')
-
-plt.tight_layout()
-
-norm = plt.Normalize(0, 1)
-sm = ScalarMappable(norm=norm, cmap=color_map)
-cbar = fig.colorbar(sm, ax=ax)
-cbar.ax.set_title('IoD')
-
-#fig.savefig(f'C:\\Users\\rsjon_000\\Documents\\point-disorder\\point_disorder_paper\\figures\\{name}.png')
-
 
 filt['iod'] = scores
 
 maj = filt['DESCRIPT'] == 'Major'
 filt['is_major'] = maj
 
-filt.boxplot('iod', 'is_major')
+filt['building_type'] = ['Major' if i else 'Other' for i in filt['is_major']]
+
+ax = filt.boxplot('iod', 'building_type')
+plt.title(f'IoD of Building Types in Lockeland Springs, Nashville, TN\n'
+          f'r={neighbor_search_dist} meters, km={ka} meters, n={coop} ')
+plt.suptitle('')
+ax.set_xlabel('Building Type')
+figout = f'C:\\Users\\rsjon_000\\Documents\\point-disorder\\point_disorder_paper\\figures\\nashvillebox.png'
+plt.savefig(figout)
+
 is_major_mean = filt[maj].iod.mean()
 other_mean = filt[~maj].iod.mean()
 
-thresh = np.average([is_major_mean, other_mean], 0, [1, 5])
+thresh = np.average([is_major_mean, other_mean], 0, [1, 3])
 classified_as_aux = filt.iod > thresh
 
 kappa = metrics.cohen_kappa_score(classified_as_aux, ~maj)
@@ -98,5 +92,47 @@ kappa = metrics.cohen_kappa_score(classified_as_aux, ~maj)
 print(f'KAPPA: {round(kappa,2)}')
 
 
-out_file = base_file + f'_SCORED_ka{ka}coop{coop}r{neighbor_search_dist}kappa{round(kappa,2)}thresh{round(thresh,2)}.shp'
+out_file = base_file + f'_SCORED_km{ka}coop{coop}r{neighbor_search_dist}kappa{round(kappa,2)}thresh{round(thresh,2)}.shp'
 filt.to_file(out_file)
+
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 8))
+color_map = cm.get_cmap('RdYlGn_r')
+im1 = ax[1].scatter(pts[:, 0], pts[:, 1], c=scores, cmap=color_map, vmin=0, vmax=1, edgecolors='black')
+ax[1].set_title(f'r={neighbor_search_dist}, ka={ka}, coop={coop}\n'
+                f'punishment={punishment}, punish_out_of_hull={punish_out_of_hull}\n'
+                f'euc={euc}, reorientation={reorientation}')
+ax[1].set_aspect('equal')
+
+norm = plt.Normalize(0, 1)
+sm = ScalarMappable(norm=norm, cmap=color_map)
+cbar = fig.colorbar(sm, ax=ax, fraction=0.02, use_gridspec=True)
+cbar.ax.set_title('IoD')
+
+assessment = [actual-classified for actual, classified in zip(~maj*2, classified_as_aux*1)]
+assessment_map = {1:'TP',
+                  -1:'FP',
+                  2:'FN',
+                  0:'TN'}
+assessment = [assessment_map[assess] for assess in assessment]
+assessment_cols_map = {'TP':'firebrick',
+                       'TN': 'darkgreen',
+                       'FP': 'limegreen',
+                       'FN': 'orange'}
+col_vec = [assessment_cols_map[assess] for assess in assessment]
+im0 = ax[0].scatter(pts[:, 0], pts[:, 1], c=col_vec, edgecolors='black')
+
+line1 = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor=assessment_cols_map['TP'], markeredgecolor='black')
+line2 = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor=assessment_cols_map['TN'], markeredgecolor='black')
+line3 = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor=assessment_cols_map['FP'], markeredgecolor='black')
+line4 = Line2D(range(1), range(1), color="white", marker='o', markerfacecolor=assessment_cols_map['FN'], markeredgecolor='black')
+lines = [line1, line2, line3, line4]
+line_names = ['True Positive', 'True Negative', 'False Positive', 'False Negative']
+
+ax[0].legend(lines, line_names, numpoints=1, loc=1, prop={'size': 12})
+ax[0].set_aspect('equal')
+
+ax[0].set_title(f'Threshold = {round(thresh,1)}\n'
+                f'κ = {round(kappa, 2)}')
+
+fig.savefig(f'C:\\Users\\rsjon_000\\Documents\\point-disorder\\point_disorder_paper\\figures\\{name}_kappa{round(kappa,2)}.png')
